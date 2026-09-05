5555597735dad8250a4dc3a7bf3f7e333aef6697
Pothunuri Laxmi Sree <pothunurilaxmisree@gmail.com>
2026-09-05 17:53:18 +0530

    Day-16 Arrays-2

---
+++ Arrays/Easy/LinearSearch.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class LinearSearch{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int index = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i]==k){
+                index=i;
+                break;
+            }
+        }
+        System.out.println(index);
+    }
+}
